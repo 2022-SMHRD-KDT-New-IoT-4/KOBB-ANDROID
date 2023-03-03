4c7184c4239fb86c5dbfcab6ba0a9d8608caea59
220in <121835105+220in@users.noreply.github.com>
2023-03-03 15:53:18 +0900

    인영 커밋 - user.png , kobb.png,choicemode

--- app/src/main/java/org/techtown/kobb/Choice_mode_page.kt
+++ app/src/main/java/org/techtown/kobb/Choice_mode_page.kt
@@ -1,11 +1,36 @@
 package org.techtown.kobb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class Choice_mode_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choice_mode_page)
+
+        val btn_Manage_main_page = findViewById<Button>(R.id.btn_Manage_main_page)
+        val btn_Start_kiosk_page = findViewById<Button>(R.id.btn_Start_kiosk_page)
+        val btn_User_info_page = findViewById<ImageButton>(R.id.btn_User_info_page)
+
+        btn_Manage_main_page.setOnClickListener{
+
+            val intent = Intent(this,Manage_main_page ::class.java)
+            startActivity(intent)
+        }
+
+        btn_Start_kiosk_page.setOnClickListener{
+            val intent = Intent(this,Start_kiosk_page::class.java)
+            startActivity(intent)
+        }
+
+        btn_User_info_page.setOnClickListener{
+            val intent = Intent(this,User_info_page::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
