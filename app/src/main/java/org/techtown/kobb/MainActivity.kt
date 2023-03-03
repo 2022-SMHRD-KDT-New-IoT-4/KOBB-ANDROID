3b626a6c5d97816b2107b4401012b6eaf95d7ecf
220in <121835105+220in@users.noreply.github.com>
2023-03-03 10:18:17 +0900

    인영 확인 커밋

--- app/src/main/java/org/techtown/kobb/MainActivity.kt
+++ app/src/main/java/org/techtown/kobb/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("hi")
+        print("확인용입니당")
     }
 }
