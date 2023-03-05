218f394c1ee2ae9668ab2676ab62b20a4f061093
whddlf0822@naver.com <whddlf0822@naver.com>
2023-03-06 08:39:13 +0900

    종일 commit

---
+++ app/src/main/java/org/techtown/kobb/Start_page.kt
@@ -0,0 +1,20 @@
+package org.techtown.kobb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Start_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+
+        Handler().postDelayed({
+            val intent = Intent(this@Start_page,
+                Login_page::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
+    }
+}
