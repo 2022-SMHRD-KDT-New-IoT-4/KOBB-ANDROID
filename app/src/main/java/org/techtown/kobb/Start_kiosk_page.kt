2edc9e2a9dcb292bf2a964ee0933c2ff26b0322f
220in <121835105+220in@users.noreply.github.com>
2023-03-06 15:02:31 +0900

    인영 커밋

---
+++ app/src/main/java/org/techtown/kobb/Start_kiosk_page.kt
@@ -0,0 +1,11 @@
+package org.techtown.kobb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start_kiosk_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_kiosk_page)
+    }
+}
