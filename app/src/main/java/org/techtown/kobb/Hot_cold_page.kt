22b4e7a674a9e2ab5c089bfabc2e422667c7528b
yunjungSeo <wdyjung@naver.com>
2023-03-06 16:03:30 +0900

    윤정 commit

---
+++ app/src/main/java/org/techtown/kobb/Hot_cold_page.kt
@@ -0,0 +1,20 @@
+package org.techtown.kobb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Hot_cold_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hot_cold_page)
+
+        // page 19 온도결정 페이지
+
+        //
+        //
+        //
+
+
+
+    }
+}
