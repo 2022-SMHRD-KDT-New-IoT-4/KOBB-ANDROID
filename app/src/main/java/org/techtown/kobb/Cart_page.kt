110ed907d33d09cb505a25b0ccf33e4e51132579
yunjungSeo <wdyjung@naver.com>
2023-03-07 09:43:13 +0900

    윤정 commit

---
+++ app/src/main/java/org/techtown/kobb/Cart_page.kt
@@ -0,0 +1,22 @@
+package org.techtown.kobb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cart_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_page)
+
+        // page 21 장바구니 페이지
+        // 뒤로가기 버튼 : btn_Small_menu_page3
+        // 장바구니 버튼 : btn_Cart_page2
+        // 장바구니 사진1 : btn_cart_page_img
+        // 장바구니 사진2 : btn_cart_page_img1
+        // 결제하기 버튼 : btn_Choice_pay_page
+        // 이전페이지, 다음페이지 이동 어떻게 해야 하지?
+
+
+
+    }
+}
--- app/src/main/java/org/techtown/kobb/Hot_cold_page.kt
+++ app/src/main/java/org/techtown/kobb/Hot_cold_page.kt
@@ -10,9 +10,12 @@ class Hot_cold_page : AppCompatActivity() {
 
         // page 19 온도결정 페이지
 
-        //
-        //
-        //
+        // 차갑게 버튼 : btn_Check_menu_page -> 메뉴확인 페이지로 이동
+        // 따뜻하게 버튼 : btn_btn_Check_menu_page -> 메뉴확인 페이지로 이동
+        // 장바구니 버튼 : btn_Cart_page -> 장바구니 페이지로 이동
+        // 뒤로 가기 버튼 : btn_Small_menu_page -> 소메뉴 내 이전페이지로 이동 / 어떤 소메뉴에 있었는지 기억하고 그 페이지로 이동이 가능할까?
+
+
 
 
 
