3d9c03d1490f9923ef2795df46e7530eaf69ef54
tmdwn19@gmail.com <tmdwn19@gmail.com>
2023-03-07 16:04:34 +0900

    승주commit

--- app/src/main/java/org/techtown/kobb/Login_page.kt
+++ app/src/main/java/org/techtown/kobb/Login_page.kt
@@ -2,10 +2,50 @@ package org.techtown.kobb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+
+
+        var login_user_id : EditText = findViewById(R.id.login_user_id)
+        var login_user_pw : EditText = findViewById(R.id.login_user_pw)
+        var btn_Choice_mode_page = findViewById<Button>(R.id.btn_Choice_mode_page)
+
+
+        val requestQueue = Volley.newRequestQueue(applicationContext)
+
+        btn_Choice_mode_page.setOnClickListener(View.OnClickListener {
+            var id = login_user_id.text.toString()
+            var pw = login_user_pw.text.toString()
+            val url="http://169.254.181.135/KOBB2/Login.do?&user_id="+id+"&user_pw="+pw
+
+            val request = StringRequest(
+                Request.Method.GET,
+                url,
+                {response->
+                    Toast.makeText(this@Login_page,"통신성공", Toast.LENGTH_SHORT).show()
+
+                },
+                { error ->
+                    Toast.makeText(this@Login_page,"통신실패", Toast.LENGTH_SHORT).show()
+                }
+
+            )
+            // post 방식의 데이터를 담을 수 있는 위치
+
+            requestQueue.add(request)
+        })
+
+
     }
 }
