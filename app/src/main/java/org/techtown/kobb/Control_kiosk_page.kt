cad512a60c07474c5c42eb2b1aa5c2a2132e6e38
yunjungSeo <wdyjung@naver.com>
2023-03-08 10:33:50 +0900

    윤정 8일 오전 commit

---
+++ app/src/main/java/org/techtown/kobb/Control_kiosk_page.kt
@@ -0,0 +1,11 @@
+package org.techtown.kobb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Control_kiosk_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_control_kiosk_page)
+    }
+}
---
+++ app/src/main/java/org/techtown/kobb/Pay_page.kt
@@ -0,0 +1,11 @@
+package org.techtown.kobb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pay_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_page)
+    }
+}
