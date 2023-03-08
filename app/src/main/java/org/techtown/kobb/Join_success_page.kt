2e98e0726a697e5021bf300117bcdbb657a15117
tmdwn19@gmail.com <tmdwn19@gmail.com>
2023-03-08 20:34:04 +0900

    승주commit

--- app/src/main/java/org/techtown/kobb/Join_success_page.kt
+++ app/src/main/java/org/techtown/kobb/Join_success_page.kt
@@ -1,11 +1,24 @@
 package org.techtown.kobb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Join_success_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join_success_page)
+
+        val btn_Login_page = findViewById<Button>(R.id.btn_Login_page)
+
+
+        btn_Login_page.setOnClickListener{
+
+            val intent = Intent(this@Join_success_page,Login_page::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
