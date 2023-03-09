e924cc7cc1247d18d5567427b87ac075a4a7a1d8
220in <121835105+220in@users.noreply.github.com>
2023-03-09 16:48:12 +0900

    인영 커밋

--- app/src/main/java/org/techtown/kobb/small_menu_page.kt
+++ app/src/main/java/org/techtown/kobb/small_menu_page.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 
 class small_menu_page : AppCompatActivity() {
@@ -19,29 +18,29 @@ class small_menu_page : AppCompatActivity() {
        eat = intent.getStringExtra("먹포")!!
 
         //상호명
-        var tvname = findViewById<TextView>(R.id.tvname)
+        var tvname = findViewById<TextView>(R.id.tvcname)
         //받아온 매장명
         user_shop_name = intent.getStringExtra("매장명")!!
         //상호명에 받아온 매장명 담아주기
         tvname.text = user_shop_name
 
-        var btn_Hot_cold_page1 = findViewById<ImageButton>(R.id.btn_Hot_cold_page1)
-        var btn_Hot_cold_page2 = findViewById<ImageButton>(R.id.btn_Hot_cold_page2)
-        //왼쪽 화면 버튼 눌렀을 때
-        btn_Hot_cold_page1.setOnClickListener{
-            val intent = Intent(this@small_menu_page,Hot_cold_page::class.java)
-            intent.putExtra("매장명",user_shop_name)
-            intent.putExtra("먹포",eat)
-            startActivity(intent)
-        }
-        //오른쪽 화면 버튼 눌렀을 때
-        btn_Hot_cold_page2.setOnClickListener{
-            val intent = Intent(this@small_menu_page,Hot_cold_page::class.java)
-            intent.putExtra("매장명",user_shop_name)
-            intent.putExtra("먹포",eat)
-            startActivity(intent)
-        }
+        var btn_Hot_cold_page1 = findViewById<Button>(R.id.btn_Hot_cold_page1)
+        var btn_Hot_cold_page2 = findViewById<Button>(R.id.btn_Hot_cold_page2)
 
+         btn_Hot_cold_page1.setOnClickListener{
+            var intent = Intent(this,Hot_cold_page::class.java )
+             intent.putExtra("매장명",user_shop_name)
+             intent.putExtra("먹포",eat)
+
+             startActivity(intent)
+         }
+
+        btn_Hot_cold_page2.setOnClickListener{
+            var intent = Intent(this,Hot_cold_page::class.java )
+            startActivity(intent)
+            intent.putExtra("매장명",user_shop_name)
+            intent.putExtra("먹포",eat)
+        }
 
 
 
